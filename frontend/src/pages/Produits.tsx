3270016480093f4b8f355f5fc79b39ccfc356d88
Romain7504 <r04.haessler@gmail.com>
2025-06-04 15:31:13 +0200

    corrections finales

--- frontend/src/pages/Produits.tsx
+++ frontend/src/pages/Produits.tsx
@@ -251,7 +251,7 @@ export default function Produits() {
                   Conditionnement
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Fournisseur
+                  TVA
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Actions
